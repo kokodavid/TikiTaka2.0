061e5236d8c895e21e8e412c4c07af75d708188f
kokodavid <kingkgdave@gmail.com>
2019-08-03 17:30:09 +0300

    continous development 03

--- app/src/main/java/com/david/tikitaka2/Constants.java
+++ app/src/main/java/com/david/tikitaka2/Constants.java
@@ -2,4 +2,8 @@ package com.david.tikitaka2;
 
 public class Constants {
 
+    public static final String EventBriteToken = BuildConfig.EventBriteToken;
+    public static final String EventBriteBaseUrl = "https://www.eventbriteapi.com/v3/events/search/?location.address=nairobi";
+
+
 }
---
+++ app/src/main/java/com/david/tikitaka2/EventBriteService.java
@@ -0,0 +1,4 @@
+package com.david.tikitaka2;
+
+public class EventBriteService {
+}
